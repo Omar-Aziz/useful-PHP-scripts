d126ffa87438e603429646fe7373bfac0a904048
Omar Bin Salamah <23618431+Omar-Aziz@users.noreply.github.com>
2018-10-18 11:28:49 -0500

    Create password_generator.php

---
+++ password_generator.php
@@ -0,0 +1,16 @@
+// Randomly generate 12 letter password - includes special characters
+// change length of password by changing number in for loop
+
+function pass_generator() {
+
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=/.';
+    $password = array(); 
+    
+    $chars_length = strlen($chars) - 1; 
+    for ($i = 0; $i < 12; $i++) {
+        $key = rand(0, $chars_length);
+        $password[] = $chars[$key];
+    }
+    return implode($password); 
+}
+echo pass_generator();
